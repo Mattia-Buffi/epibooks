a3692b0bdfe4028d72e33659c0c9f2d9e0854dd1
Mattia Buffello <buffello@me.com>
2024-03-08 16:24:45 +0100

    col/row

--- src/componenti/AllTheBooks/AllTheBooks.jsx
+++ src/componenti/AllTheBooks/AllTheBooks.jsx
@@ -7,16 +7,16 @@ export default function AllTheBooks() {
     let topHorror=horrorBooks.slice(0,5);
     let topFantasy=fantasyBooks.slice(0,5);
     return (
-    <div>
+    <div className='container-fluid'>
         <div>
         <h2>Top 5: Horror</h2>
-        <div className='d-flex justify-content-between'>
+        <div className='row g-2'>
         {topHorror.map((book)=><CardBook key={book.asin} book={book}/>)}
         </div>
         </div>
         <div>
         <h2>Top 5: Fantasy</h2>
-        <div className='d-flex justify-content-between'>
+        <div className='row g-2'>
         {topFantasy.map((book)=><CardBook key={book.asin} book={book}/>)}
         </div>
         </div>
--- src/componenti/CardBook/CardBook.jsx
+++ src/componenti/CardBook/CardBook.jsx
@@ -4,9 +4,9 @@ import { Card,Button } from 'react-bootstrap';
 export default function CardBook({book}) {
     let {title,img,price}=book;
     return (
-    <Card className='m-2' style={{ width: '16rem'}}>
-      <Card.Img variant="top" style={{ height: '18rem' }} src={img} />
-      <Card.Body className='d-flex flex-column h-100 justify-content-between'>
+    <Card className='m-2 col' style={{ width: '16rem',minWidth:'150px'}}>
+      <Card.Img variant="top" style={{ height: '60%' }} src={img} />
+      <Card.Body className='d-flex flex-column justify-content-between'>
         <Card.Title>{title}</Card.Title>
         <Button variant="primary">Buy for $ {price}</Button>
       </Card.Body>
