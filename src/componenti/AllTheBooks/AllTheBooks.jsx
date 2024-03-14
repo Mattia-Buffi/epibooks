ea68efc2f6d9e1d1bddff2acb43562380d1cdd58
Mattia Buffello <buffello@me.com>
2024-03-14 15:56:20 +0100

    ricerca e focus

--- src/componenti/AllTheBooks/AllTheBooks.jsx
+++ src/componenti/AllTheBooks/AllTheBooks.jsx
@@ -2,21 +2,49 @@ import React from 'react';
 import fantasyBooks from '../books/fantasy.json';
 import horrorBooks from '../books/horror.json';
 import CardBook from '../CardBook/CardBook';
+import { Form,Button } from 'react-bootstrap';
+import { useState } from 'react';
 
 export default function AllTheBooks() {
     let topHorror=horrorBooks.slice(0,5);
-    let topFantasy=fantasyBooks.slice(0,5);
+    let topFantasy=fantasyBooks.slice(0,10);
+
+    const [inputName,setInputName]=useState('');
+    const [listBook,setListBook]=useState([]);
+
+    function filterBooks(e){
+        e.preventDefault();
+        setListBook(fantasyBooks.filter((book)=>book.title.toLocaleLowerCase().includes(inputName.toLocaleLowerCase())))
+    }
+    function deleteList(){
+      setListBook([]);
+    }
     return (
     <div className='container-fluid'>
+        <div className='d-flex justify-content-center m-2'>
+        <Form className="d-flex w-75 align-items-baseline">
+            <Form.Label className='me-2'>Cerca</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className="me-2"
+              value={inputName}
+              onChange={(e)=>setInputName(e.target.value)}
+            />
+            <Button variant="outline-success" onClick={(e)=>filterBooks(e)}>Search</Button>
+            {(inputName!=0)&&<Button className='ms-2' variant="outline-danger" onClick={deleteList}>Reset</Button>}
+        </Form>
+        </div>
         <div>
-        <h2>Top 5: Horror</h2>
-        <div className='row g-2'>
-        {topHorror.map((book)=><CardBook key={book.asin} book={book}/>)}
+        <h2>Ricerca</h2>
+        <div className='row justify-content-center'>
+        {listBook.map((book)=><CardBook key={book.asin} book={book}/>)}
         </div>
         </div>
+
         <div>
-        <h2>Top 5: Fantasy</h2>
-        <div className='row g-2'>
+        <h2>Top 10: Fantasy</h2>
+        <div className='row justify-content-center'>
         {topFantasy.map((book)=><CardBook key={book.asin} book={book}/>)}
         </div>
         </div>
--- src/componenti/CardBook/CardBook.jsx
+++ src/componenti/CardBook/CardBook.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Card,Button } from 'react-bootstrap';
+import { useState } from 'react';
+import './CardBook.css';
 
 export default function CardBook({book}) {
     let {title,img,price}=book;
+    const[selected,setSelected]=useState(false);
+    function selectBook(){
+      setSelected(!selected);
+    }
     return (
-    <Card className='m-2 col' style={{ width: '16rem',minWidth:'150px'}}>
+    <Card className={selected?'m-2 col-3 focusCard cardsel':'m-2 col-3 focusCard'} 
+          style={{ width: '16rem',minWidth:'150px'}}
+          onClick={selectBook}>
       <Card.Img variant="top" style={{ height: '60%' }} src={img} />
       <Card.Body className='d-flex flex-column justify-content-between'>
         <Card.Title>{title}</Card.Title>
